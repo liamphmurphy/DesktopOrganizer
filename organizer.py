f5f76a156082396e22bfb380e7f4b3f3554cac00
murnux <liammurphy@fastmail.com>
2017-12-18 16:43:43 -0800

    Files now move, only into Documents though. Progress!

--- organizer.py
+++ organizer.py
@@ -1,33 +1,43 @@
 import os, sys, shutil
 from os.path import expanduser, splitext
-from collections import defaultdict
 import settings
+from collections import defaultdict
 
-#os.mkdir(home+"/Test",mode=0o777)
+#print(settings.file_extensions.items())
+#for directory, extensions in settings.file_extensions.items():
+#    print(directory, extensions)
 
 class Directories:
 
-    def create_directories(user_desktop):
-        #settings.file_extensions.items() = defaultdict(list)
-        for folder, extensions in settings.file_extensions.items():
-            os.mkdir(user_desktop+"/"+folder, mode=0o777)
+    def create_directories(user_desktop, files):
+        #path_extensions = defaultdict(list)
+        for directory, extensions in settings.file_extensions.items():
+            print(directory)
+            directory_path = user_desktop+"/"+directory
+            os.mkdir(directory_path, mode=0o777)
             print("\nDirectories successfully added to your desktop.")
-        for folder, extensions in settings.file_extensions.items():
-            os.chdir(user_desktop)
-            extensions = str(extensions)
-            file_path = (folder+"/"+extensions)
-            shutil.move(extensions, file_path)
-            
 
+
+        for file in files:
+            file_path = directory_path + "/" + file
+            print(files)
+            print(file_path)
+            shutil.move(user_desktop + "/" + file, directory_path)
+
+            
     # For now, this is for testing purposes: quickly remove dirs so we don't have to manually do it every time.
     def remove_directories(user_desktop):
-        for folder, extensions in settings.file_extensions.items():
-            os.rmdir(user_desktop+"/"+folder)
+        for directory in settings.file_extensions.keys():
+            try:
+                os.rmdir(user_desktop+"/"+directory)
+            except:
+                pass
 
 def main():
     main_program = Directories()
     user_desktop = expanduser("~/Desktop")
-    Directories.create_directories(user_desktop)
+    files = [f for f in os.listdir(user_desktop) if os.path.isfile(os.path.join(user_desktop, f))]
+    Directories.create_directories(user_desktop,files)
 
 if __name__ == "__main__":
     main()
